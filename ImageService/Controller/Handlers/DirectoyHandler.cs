480988f204303775898a653e2756f168ef63219a
udigoldman1 <udigoldman1@gmail.com>
2018-04-10 18:36:39 +0300

    10.4 18:36

--- ImageService/Controller/Handlers/DirectoyHandler.cs
+++ ImageService/Controller/Handlers/DirectoyHandler.cs
@@ -20,10 +20,59 @@ namespace ImageService.Controller.Handlers
         private ILoggingService m_logging;
         private FileSystemWatcher m_dirWatcher;             // The Watcher of the Dir
         private string m_path;                              // The Path of directory
+        private string[] extentions = { ".jpg", ".png", ".gif", ".bmp" };
         #endregion
 
         public event EventHandler<DirectoryCloseEventArgs> DirectoryClose;              // The Event That Notifies that the Directory is being closed
 
-		// Implement Here!
+        public DirectoyHandler(string path, ImageController controller, ILoggingService logging)
+        {
+            this.m_controller = controller;
+            this.m_path = path;
+            this.m_logging = logging;
+            this.m_dirWatcher = new FileSystemWatcher(path);
+        }
+
+        // The Function Recieves the directory to Handle
+        public void StartHandleDirectory(string dirPath)
+        {
+            this.m_dirWatcher.Created += new FileSystemEventHandler(OnFileMoved);
+            this.m_dirWatcher.EnableRaisingEvents = true;
+        }
+
+        private void OnFileMoved(object source, FileSystemEventArgs e)
+        {
+            string[] args = new string[1];
+            args[0] = this.m_path;
+
+            string extention = Path.GetExtension(e.FullPath);
+            foreach (string ex in this.extentions)
+            {
+                if (ex == extention)
+                {
+                    OnCommandRecieved(this, new CommandRecievedEventArgs(0, args, this.m_path));
+                }
+            }  
+        }
+
+        public void OnCommandRecieved(object sender, CommandRecievedEventArgs e)
+        {
+            if (e.CommandID == 0)
+            {
+                bool result;
+                string messsage = this.m_controller.ExecuteCommand(e.CommandID, e.Args, out result);
+                if (result)
+                {
+                    m_logging.Log(messsage, MessageTypeEnum.INFO);
+                }
+                else
+                {
+                    m_logging.Log(messsage, MessageTypeEnum.FAIL);
+                }
+            } else if (e.CommandID == 1)
+            {
+                m_logging.Log("Handler closing", MessageTypeEnum.INFO);
+            }
+        }
     }
 }
